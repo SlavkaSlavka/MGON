a1f3c89d48dabd3397f5587648ddbf484f05bc06
DmytroLutsenko <dmytro.lutsenko@form.com>
2022-10-16 23:48:34 +0300

    modal refactoring

--- js/modal.js
+++ js/modal.js
@@ -1,12 +1,19 @@
 // modal
 
 (() => {
-    $('body').on('click', '.photo-btn', () => {
+    $('body').on('click', '.photo-btn', (e) => {
         const modalForm = $('#modal_form');
-        const submitBtn = $(this);
+        const submitBtn = $(e.target);
+        const card = submitBtn.closest('.column');
+        const name = card.attr('data-name');
+        const price = card.attr('data-price');
+        const set = card.attr('data-set');
+
+        modalForm.find('.modal__name_description').text(name);
+        modalForm.find('.modal__set_description').text(set);
+        modalForm.find('.modal__cost_description').text(price);
 
         modalForm.modal('show');
-
     });
 })();
 
